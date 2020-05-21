5ad6bffe387086038cabc28f141425b36145b8fd
xiaohai <xiaohei@qq.com>
2020-05-21 17:12:39 +0800

    添加页面

--- src/main/java/com/sql_zsgc/control/Zsgc.java
+++ src/main/java/com/sql_zsgc/control/Zsgc.java
@@ -4,18 +4,28 @@ package com.sql_zsgc.control;
 import com.sql_zsgc.Repository.SzygdlRepository;
 import com.sql_zsgc.pojo.Szygdl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-@RestController
+@Controller
 public class Zsgc {
 
 
     @Autowired
     private SzygdlRepository szygdlRepository;
 
+    @RequestMapping("info")
+    public String getinfo(Model model) {
+        List<Szygdl> resultList = szygdlRepository.findAll();
+        model.addAttribute("resultList",resultList);
+        return "materialInfo";
+    }
 
     @GetMapping("getData")
     public List<Szygdl> getData() {
@@ -23,6 +33,12 @@ public class Zsgc {
         return all;
     }
 
+    @GetMapping("getOneData")
+    public Szygdl getOneData() {
+        List<Szygdl> all = szygdlRepository.findAll();
+        Szygdl szygdl = all.get(0);
+        return szygdl;
+    }
 
 }
 
